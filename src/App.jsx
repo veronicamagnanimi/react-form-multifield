99e17b38699dca4962abe85e6a3262329002dd7c
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-12-18 17:20:49 +0100

    feat: published with use effect & image input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const initalForm = {
   title: "",
@@ -13,6 +13,18 @@ function App() {
 
   const [activeArticles, setActiveArticles] = useState([]);
   const [formData, setFormData] = useState(initalForm);
+
+  const [publishedMessage, setPublishedMessage] = useState("");
+
+  //USE EFFECT
+  useEffect(() => {
+    if (formData.published) {
+      setPublishedMessage("Article visible")
+     } else {
+       setPublishedMessage("Article not visible")
+     }
+   
+  }, [formData.published])
 
 
   //FUNZIONE EVENTO BOTTONE
@@ -79,6 +91,8 @@ const handleInputChange = (event) => {
         )}
        
        {/* FORM */}
+
+       {/*Title*/}
         <h2 className="text-secondary mb-3">Insert a new article here</h2>
         <form action="" onSubmit={handleArticlesForm}>
           <div className="mb-3">
@@ -87,22 +101,25 @@ const handleInputChange = (event) => {
             name="title" value={formData.name} onChange={handleInputChange} />
           </div>
 
+         {/*Author*/}
           <div className="mb-3">
             <label htmlFor="author" className="text-secondary"><strong>Enter author</strong></label>
             <input type="text" id="author" className="form-control"
             name="author" value={formData.author}
               onChange={handleInputChange}/>
           </div>
-
+         
+         {/*Description*/}
           <div className="mb-3">
             <label htmlFor="description" className="text-secondary"><strong>Enter description</strong></label>
             <input type="text" id="description" className="form-control"
             name="description" value={formData.description}
               onChange={handleInputChange}/>
           </div>
-
+          
+          {/*Category*/}
           <div className="mb-3">
-          <select className="form-select text-secondary " aria-label="Default select example" name="category"
+          <select className="form-select text-secondary fw-bold" aria-label="Default select example" name="category"
            value={formData.category} onChange={handleInputChange}>
            <option selected>Category</option>
           <option value="Category 1">One</option>
@@ -110,13 +127,24 @@ const handleInputChange = (event) => {
           <option value="Category 3">Three</option>
          </select>
           </div>
+         
+         {/*Published*/}
+          <div className="mb-3">
+              <label htmlFor="published" className="text-secondary fw-bold me-2">Published</label>
+              <input id="published" type="checkbox" name="published" checked={formData.published}
+              onChange={handleInputChange}
+              />
+              <div>{publishedMessage}</div>
+            </div>
 
-
-
-
-
-
-          <button type="submit" className="btn btn-secondary mt-3">Add Article</button>
+            {/*Image*/}
+            <div className="mb-3">
+              <label htmlFor="image" className="text-secondary fw-bold me-2">Image</label>
+              <input id="image" type="text" name="image" value={formData.image}
+              onChange={handleInputChange}
+              />
+            </div>
+           <button type="submit" className="btn btn-secondary mt-3">Add Article</button>
         </form>
       </div>
     </>
